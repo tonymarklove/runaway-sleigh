c81abb90aa33672489b6bd6557b0ef4d8d57b73d
Tony Marklove <tony@fluxbit.co.uk>
2016-12-27 13:35:48 +0000

    Reset the gamestate to restart level

--- main.js
+++ main.js
@@ -18,25 +18,9 @@ var mainExports = (function() {
   var TURN_RATE = 3; // degrees per frame
 
   // State
-  var gameState = {
-    phaser: null,
-    background: null,
-    groundGroup: null,
-    overlayGroup: null,
-    houses: [],
-    turnSounds: [],
-    beepSounds: [],
-    screenOverlay: null,
-    elapsedMs: 0,
-    timerText: null,
-    deliveredText: null,
-    player: {
-      turnDirection: 'left',
-      delivered: 0,
-      deliveredInARow: 0,
-      currentPresentImage: null
-    }
-  };
+  var gameState = null;
+
+  var phaser = null;
 
   var player = null;
   var cursors = null;
@@ -84,35 +68,57 @@ var mainExports = (function() {
   var HOUSE_COUNT = houseData.length;
 
   function preload() {
-    gameState.phaser.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
+    phaser.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
 
-    gameState.phaser.load.image('background','assets/map-unfound.jpg');
-    gameState.phaser.load.atlas('lit_houses', 'assets/lit-houses.png', 'assets/house-atlas.json');
-    gameState.phaser.load.image('player','assets/ship.png');
-    gameState.phaser.load.image('santa','assets/santa.png');
-    gameState.phaser.load.image('clock','assets/clock.png');
-    gameState.phaser.load.image('overlay','assets/overlay.png');
-    gameState.phaser.load.image('logo','assets/logo.png');
-    gameState.phaser.load.spritesheet('play_again_button','assets/play-again.png', 202, 50);
+    phaser.load.image('background','assets/map-unfound.jpg');
+    phaser.load.atlas('lit_houses', 'assets/lit-houses.png', 'assets/house-atlas.json');
+    phaser.load.image('player','assets/ship.png');
+    phaser.load.image('santa','assets/santa.png');
+    phaser.load.image('clock','assets/clock.png');
+    phaser.load.image('overlay','assets/overlay.png');
+    phaser.load.image('logo','assets/logo.png');
+    phaser.load.spritesheet('play_again_button','assets/play-again.png', 202, 50);
 
-    gameState.phaser.load.image('present_blue','assets/present-blue.png');
-    gameState.phaser.load.image('present_green','assets/present-green.png');
-    gameState.phaser.load.image('present_purple','assets/present-purple.png');
-    gameState.phaser.load.image('present_red','assets/present-red.png');
-    gameState.phaser.load.image('present_yellow','assets/present-yellow.png');
+    phaser.load.image('present_blue','assets/present-blue.png');
+    phaser.load.image('present_green','assets/present-green.png');
+    phaser.load.image('present_purple','assets/present-purple.png');
+    phaser.load.image('present_red','assets/present-red.png');
+    phaser.load.image('present_yellow','assets/present-yellow.png');
 
-    gameState.phaser.load.image('star_particle1','assets/star.png');
-    gameState.phaser.load.image('star_particle2','assets/star_particle.png');
+    phaser.load.image('star_particle1','assets/star.png');
+    phaser.load.image('star_particle2','assets/star_particle.png');
 
-    gameState.phaser.load.audio('jingle_bells', ['assets/jingle-bells.mp3']);
-    gameState.phaser.load.audio('turn1', ['assets/turn1.mp3']);
-    gameState.phaser.load.audio('turn2', ['assets/turn2.mp3']);
+    phaser.load.audio('jingle_bells', ['assets/jingle-bells.mp3']);
+    phaser.load.audio('turn1', ['assets/turn1.mp3']);
+    phaser.load.audio('turn2', ['assets/turn2.mp3']);
 
-    gameState.phaser.load.audio('beep1', ['assets/beep1.mp3']);
-    gameState.phaser.load.audio('beep2', ['assets/beep2.mp3']);
-    gameState.phaser.load.audio('beep3', ['assets/beep3.mp3']);
-    gameState.phaser.load.audio('beep4', ['assets/beep4.mp3']);
-    gameState.phaser.load.audio('beep5', ['assets/beep5.mp3']);
+    phaser.load.audio('beep1', ['assets/beep1.mp3']);
+    phaser.load.audio('beep2', ['assets/beep2.mp3']);
+    phaser.load.audio('beep3', ['assets/beep3.mp3']);
+    phaser.load.audio('beep4', ['assets/beep4.mp3']);
+    phaser.load.audio('beep5', ['assets/beep5.mp3']);
+  }
+
+  function resetGameState() {
+    gameState = {
+        background: null,
+        music: null,
+        groundGroup: null,
+        overlayGroup: null,
+        houses: [],
+        turnSounds: [],
+        beepSounds: [],
+        screenOverlay: null,
+        elapsedMs: 0,
+        timerText: null,
+        deliveredText: null,
+        player: {
+          turnDirection: 'left',
+          delivered: 0,
+          deliveredInARow: 0,
+          currentPresentImage: null
+        }
+      }
   }
 
   function pickPresentColor() {
@@ -128,7 +134,7 @@ var mainExports = (function() {
   }
 
   function createTimer() {
-    var text = gameState.phaser.add.text(0, 0, "0 : 00");
+    var text = phaser.add.text(0, 0, "0 : 00");
     text.anchor.setTo(0.5);
 
     text.font = 'Fontdiner Swanky';
@@ -149,7 +155,7 @@ var mainExports = (function() {
     text.strokeThickness = 2;
     text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
 
-    var clock = gameState.phaser.add.sprite(0,0, 'clock');
+    var clock = phaser.add.sprite(0,0, 'clock');
     clock.anchor.setTo(0.5);
     clock.fixedToCamera = true;
     clock.cameraOffset.setTo(40, 40);
@@ -159,7 +165,7 @@ var mainExports = (function() {
   }
 
   function createBlueText(text, x, y) {
-    var text = gameState.phaser.add.text(0, 0, text);
+    var text = phaser.add.text(0, 0, text);
     text.anchor.setTo(0.5);
 
     text.font = 'Fontdiner Swanky';
@@ -185,7 +191,7 @@ var mainExports = (function() {
   function createDeliveredUi() {
     var text = createBlueText("0", SCREEN_WIDTH - 100, 50);
 
-    var presentIcon = gameState.phaser.add.sprite(0,0, pickPresentColor());
+    var presentIcon = phaser.add.sprite(0,0, pickPresentColor());
     presentIcon.anchor.setTo(0.5);
     presentIcon.fixedToCamera = true;
     presentIcon.cameraOffset.setTo(SCREEN_WIDTH - 230, 45);
@@ -195,7 +201,8 @@ var mainExports = (function() {
   }
 
   function actionOnClickPlayAgain() {
-    window.location.reload();
+    gameState.music.stop();
+    phaser.state.restart();
   }
 
   function createScoreboardText(timeForThisGame, topFiveTimes) {
@@ -205,7 +212,7 @@ var mainExports = (function() {
       var topTime = topFiveTimes[i];
       var topTimeText = createTimeText(topTime);
       var y = 250 + (i * 50);
-      var text = gameState.phaser.add.text(SCREEN_WIDTH / 2, y, topTimeText);
+      var text = phaser.add.text(SCREEN_WIDTH / 2, y, topTimeText);
       text.anchor.setTo(0.5, 0);
       // text.font = 'Fontdiner Swanky';
       text.fontSize = 40;
@@ -225,21 +232,21 @@ var mainExports = (function() {
       text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
     }
 
-    var button = gameState.phaser.add.button(SCREEN_WIDTH / 2, 520, 'play_again_button', actionOnClickPlayAgain, this, 2, 1, 0);
+    var button = phaser.add.button(SCREEN_WIDTH / 2, 520, 'play_again_button', actionOnClickPlayAgain, this, 2, 1, 0);
     button.anchor.setTo(0.5, 0);
     button.fixedToCamera = true;
   }
 
   function createScoreboard(timeForThisGame) {
-    gameState.screenOverlay = gameState.phaser.add.tileSprite(0,0, SCREEN_WIDTH, SCREEN_HEIGHT, 'overlay');
+    gameState.screenOverlay = phaser.add.tileSprite(0,0, SCREEN_WIDTH, SCREEN_HEIGHT, 'overlay');
     gameState.screenOverlay.fixedToCamera = true;
     gameState.screenOverlay.alpha = 0.7;
 
-    var logo = gameState.phaser.add.sprite(SCREEN_WIDTH / 2, 40, 'logo');
+    var logo = phaser.add.sprite(SCREEN_WIDTH / 2, 40, 'logo');
     logo.anchor.setTo(0.5, 0);
     logo.scale.setTo(0.75);
 
-    gameState.overlayGroup = gameState.phaser.add.group();
+    gameState.overlayGroup = phaser.add.group();
     gameState.overlayGroup.fixedToCamera = true;
     gameState.overlayGroup.add(logo);
 
@@ -253,7 +260,7 @@ var mainExports = (function() {
       var houseDatum = houseData[i];
       var chimney = houseDatum.chimney;
       var litPosition = houseDatum.litSpritePos;
-      var litSprite = gameState.phaser.add.sprite(litPosition.x,litPosition.y, 'lit_houses');
+      var litSprite = phaser.add.sprite(litPosition.x,litPosition.y, 'lit_houses');
 
       litSprite.anchor.setTo(0.5, 0.5);
       litSprite.frameName = houseDatum.frameName;
@@ -281,7 +288,7 @@ var mainExports = (function() {
     presentVelocity.multiply(time / 1000, time / 1000);
 
     var targetPoint = Phaser.Point.add(currentPos, presentVelocity);
-    var targetAngle = gameState.phaser.rnd.angle();
+    var targetAngle = phaser.rnd.angle();
     var hitHouse = houseWasHit(targetPoint);
 
     if (hitHouse) {
@@ -290,14 +297,14 @@ var mainExports = (function() {
       targetAngle = chimney.angle;
     }
 
-    present.sprite = gameState.phaser.add.sprite(currentPos.x, currentPos.y, gameState.player.currentPresentImage);
+    present.sprite = phaser.add.sprite(currentPos.x, currentPos.y, gameState.player.currentPresentImage);
     present.sprite.anchor.setTo(0.5, 0.5);
     gameState.groundGroup.add(present.sprite);
 
-    gameState.phaser.add.tween(present.sprite).to({ x: targetPoint.x, y: targetPoint.y, angle: targetAngle }, time, Phaser.Easing.Linear.None, true, 0);
+    phaser.add.tween(present.sprite).to({ x: targetPoint.x, y: targetPoint.y, angle: targetAngle }, time, Phaser.Easing.Linear.None, true, 0);
 
-    var scale1 = gameState.phaser.add.tween(present.sprite.scale).to({x: 1.8, y: 1.8}, time * 0.3, Phaser.Easing.Linear.None)
-    var scale2 = gameState.phaser.add.tween(present.sprite.scale).to({x: 0.8, y: 0.8}, time * 0.7, Phaser.Easing.Linear.None);
+    var scale1 = phaser.add.tween(present.sprite.scale).to({x: 1.8, y: 1.8}, time * 0.3, Phaser.Easing.Linear.None)
+    var scale2 = phaser.add.tween(present.sprite.scale).to({x: 0.8, y: 0.8}, time * 0.7, Phaser.Easing.Linear.None);
 
     scale1.chain(scale2);
 
@@ -322,12 +329,13 @@ var mainExports = (function() {
   }
 
   function create() {
-    gameState.background = gameState.phaser.add.tileSprite(0, 0, 2160, 2160, 'background');
-    gameState.phaser.world.setBounds(0, 0, 2160, 2160);
+    resetGameState();
+    gameState.background = phaser.add.tileSprite(0, 0, 2160, 2160, 'background');
+    phaser.world.setBounds(0, 0, 2160, 2160);
 
-    gameState.groundGroup = gameState.phaser.add.group();
+    gameState.groundGroup = phaser.add.group();
 
-    emitter = gameState.phaser.add.emitter(gameState.phaser.world.centerX, gameState.phaser.world.centerY, 400);
+    emitter = phaser.add.emitter(phaser.world.centerX, phaser.world.centerY, 400);
     emitter.makeParticles(['star_particle1', 'star_particle2']);
 
     emitter.gravity = 0;
@@ -335,25 +343,25 @@ var mainExports = (function() {
     emitter.setScale(0.8, 0, 0.8, 0, 3000);
     emitter.start(false, 3000, 5);
 
-    presentEmitter = gameState.phaser.add.emitter(gameState.phaser.world.centerX, gameState.phaser.world.centerY, 400);
+    presentEmitter = phaser.add.emitter(phaser.world.centerX, phaser.world.centerY, 400);
     presentEmitter.makeParticles(['present_blue', 'present_green', 'present_purple', 'present_red', 'present_yellow']);
     presentEmitter.setAlpha(1, 0, 3000);
     presentEmitter.setScale(1.5, 0, 0.8, 0, 3000);
 
-    gameState.phaser.physics.startSystem(Phaser.Physics.ARCADE);
+    phaser.physics.startSystem(Phaser.Physics.ARCADE);
 
-    player = gameState.phaser.add.sprite(gameState.phaser.world.centerX, gameState.phaser.world.centerY, 'player');
+    player = phaser.add.sprite(phaser.world.centerX, phaser.world.centerY, 'player');
     player.anchor.setTo(0.5, 0.5);
-    gameState.phaser.physics.arcade.enable(player);
+    phaser.physics.arcade.enable(player);
     player.body.collideWorldBounds = true;
 
     gameState.player.currentPresentImage = pickPresentColor();
-    present = gameState.phaser.make.sprite(5,-35, gameState.player.currentPresentImage);
+    present = phaser.make.sprite(5,-35, gameState.player.currentPresentImage);
     present.anchor.setTo(0.5, 0.5);
     present.angle = -90;
     player.addChild(present);
 
-    santa = gameState.phaser.make.sprite(0,-12, 'santa');
+    santa = phaser.make.sprite(0,-12, 'santa');
     santa.anchor.setTo(0.5, 0.5);
     player.addChild(santa);
 
@@ -361,21 +369,21 @@ var mainExports = (function() {
     createTimer();
     createDeliveredUi();
 
-    cursors = gameState.phaser.input.keyboard.createCursorKeys();
-    spaceKey = gameState.phaser.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    cursors = phaser.input.keyboard.createCursorKeys();
+    spaceKey = phaser.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    music = gameState.phaser.add.audio('jingle_bells', 0.25);
-    gameState.turnSounds.push(gameState.phaser.add.audio('turn1'));
-    gameState.turnSounds.push(gameState.phaser.add.audio('turn2'));
+    gameState.music = phaser.add.audio('jingle_bells', 0.25);
+    gameState.turnSounds.push(phaser.add.audio('turn1'));
+    gameState.turnSounds.push(phaser.add.audio('turn2'));
 
-    gameState.beepSounds.push(gameState.phaser.add.audio('beep1'));
-    gameState.beepSounds.push(gameState.phaser.add.audio('beep2'));
-    gameState.beepSounds.push(gameState.phaser.add.audio('beep3'));
-    gameState.beepSounds.push(gameState.phaser.add.audio('beep4'));
-    gameState.beepSounds.push(gameState.phaser.add.audio('beep5'));
-    music.play();
+    gameState.beepSounds.push(phaser.add.audio('beep1'));
+    gameState.beepSounds.push(phaser.add.audio('beep2'));
+    gameState.beepSounds.push(phaser.add.audio('beep3'));
+    gameState.beepSounds.push(phaser.add.audio('beep4'));
+    gameState.beepSounds.push(phaser.add.audio('beep5'));
+    gameState.music.play();
 
-    gameState.phaser.camera.roundPx = false;
+    phaser.camera.roundPx = false;
   }
 
   function padLeft(nr, n, str){
@@ -401,7 +409,7 @@ var mainExports = (function() {
   function chimneyForHouse(house) {
     return {
       position: house.chimney,
-      angle: gameState.phaser.rnd.angle()
+      angle: phaser.rnd.angle()
     };
   }
 
@@ -416,14 +424,14 @@ var mainExports = (function() {
 
     var lerp = 0.05;
 
-    gameState.phaser.camera.x = gameState.phaser.math.linear(
-      gameState.phaser.camera.x,
-      targetPosition.x - gameState.phaser.camera.view.halfWidth,
+    phaser.camera.x = phaser.math.linear(
+      phaser.camera.x,
+      targetPosition.x - phaser.camera.view.halfWidth,
       lerp
     );
-    gameState.phaser.camera.y = gameState.phaser.math.linear(
-      gameState.phaser.camera.y,
-      targetPosition.y - gameState.phaser.camera.view.halfHeight,
+    phaser.camera.y = phaser.math.linear(
+      phaser.camera.y,
+      targetPosition.y - phaser.camera.view.halfHeight,
       lerp
     );
   }
@@ -458,7 +466,7 @@ var mainExports = (function() {
   }
 
   function update() {
-    gameState.elapsedMs += gameState.phaser.time.physicsElapsedMS;
+    gameState.elapsedMs += phaser.time.physicsElapsedMS;
 
     var turnRate = TURN_RATE + (0.1 * gameState.player.deliveredInARow);
 
@@ -526,7 +534,7 @@ var mainExports = (function() {
   function main() {
     var phaserFunctions = { preload: preload, create: create, update: update };
 
-    gameState.phaser = new Phaser.Game(
+    phaser = new Phaser.Game(
       SCREEN_WIDTH,
       SCREEN_HEIGHT,
       Phaser.AUTO,
